ca2cead52fbc66ec22aa8229a97217714f9a45ac
ChanNaru <narutyan0711@gmail.com>
2023-03-03 22:57:54 +0900

    update md

---
+++ design-pattern/adapter-pattern/java-sample-using-delegate/PrintBanner.java
@@ -0,0 +1,17 @@
+public class PrintBanner extends Print {
+    private Banner banner;
+
+    public PrintBanner(String string) {
+        this.banner = new Banner(string);
+    }
+
+    @Override
+    public void printWeak() {
+        this.banner.showWithParen();
+    }
+
+    @Override
+    public void printStrong() {
+        this.banner.showWithAster();
+    }
+}
