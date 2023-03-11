f27d5d36c4fd1580e8c139f2cb5b0c881b263421
ChanNaru <narutyan0711@gmail.com>
2023-03-11 10:27:35 +0900

    update md

--- design-pattern/07-builder-pattern/HTMLBuilder.java
+++ design-pattern/07-builder-pattern/HTMLBuilder.java
@@ -2,43 +2,43 @@ import java.io.FileWriter;
 import java.io.Writer;
 
 public class HTMLBuilder extends Builder {
-    private String filename = "untitled.html";
+    private String filename = "./untitled.html";
     private StringBuilder sb = new StringBuilder();
 
     @Override
     public void makeTitle(String title) {
-        filename = title + ".html";
-        this.sb.append("<html>");
-        this.sb.append("<body>");
+        filename = "./" + title + ".html";
+        this.sb.append("<html>\n");
+        this.sb.append("<body>\n");
         this.sb.append("<h1>");
         this.sb.append(title);
-        this.sb.append("</h1>");
+        this.sb.append("</h1>\n");
     }
 
     @Override
     public void makeString(String str) {
         this.sb.append("<p>");
         this.sb.append(str);
-        this.sb.append("</p>");
+        this.sb.append("</p>\n");
     }
 
     @Override
     public void makeItems(String[] items) {
-        this.sb.append("<ul>");
+        this.sb.append("<ul>\n");
         for (String s : items) {
             this.sb.append("<li>");
             this.sb.append(s);
-            this.sb.append("</li>");
+            this.sb.append("</li>\n");
         }
-        this.sb.append("</ul>");
+        this.sb.append("</ul>\n");
     }
 
     @Override
     public void close() {
-        this.sb.append("</body>");
-        this.sb.append("</html>");
+        this.sb.append("</body>\n");
+        this.sb.append("</html>\n");
         try {
-            Writer writer = new FileWriter(filename);
+            Writer writer = new FileWriter(this.filename);
             writer.write(sb.toString());
             writer.close();
         } catch (Exception e) {
