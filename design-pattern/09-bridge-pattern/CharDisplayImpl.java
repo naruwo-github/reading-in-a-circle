269eede59bf6532d4e7e1218386054e117db1efe
ChanNaru <narutyan0711@gmail.com>
2023-03-11 23:06:01 +0900

    update md

---
+++ design-pattern/09-bridge-pattern/CharDisplayImpl.java
@@ -0,0 +1,26 @@
+public class CharDisplayImpl extends DisplayImpl {
+    private char head;
+    private char body;
+    private char foot;
+
+    public CharDisplayImpl(char head, char body, char foot) {
+        this.head = head;
+        this.body = body;
+        this.foot = foot;
+    }
+
+    @Override
+    public void rawOpen() {
+        System.out.print(this.head);
+    }
+
+    @Override
+    public void rawPrint() {
+        System.out.print(this.body);
+    }
+
+    @Override
+    public void rawClose() {
+        System.out.print(this.foot);
+    }
+}
---
+++ design-pattern/09-bridge-pattern/FileDisplayImpl.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileDisplayImpl extends DisplayImpl {
+    private String fileName;
+
+    public FileDisplayImpl(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void rawOpen() {
+        System.out.println("=== " + this.fileName + " ===");
+    }
+
+    @Override
+    public void rawPrint() {
+        try {
+            for (String line : Files.readAllLines(Path.of(this.fileName))) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void rawClose() {
+        System.out.println("=========");
+    }
+}
