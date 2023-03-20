0755ec1772f8e37e20e933cb9aee7788df783c3b
ChanNaru <narutyan0711@gmail.com>
2023-03-21 08:23:49 +0900

    update md

---
+++ design-pattern/19-state-pattern/Context.java
@@ -0,0 +1,9 @@
+public interface Context {
+    public abstract void setClock(int hour);
+
+    public abstract void changeState(State state);
+
+    public abstract void callSecurityCenter(String message);
+
+    public abstract void recordLog(String message);
+}
