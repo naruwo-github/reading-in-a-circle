09839c06131118d9694fc46d69470eee09a575ca
ChanNaru <narutyan0711@gmail.com>
2023-04-30 22:31:35 +0900

    launch flask app

---
+++ web-systems/api-flask-mysql-docker-kubernetes/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+api = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.environ['DB_USERNAME']}:{os.environ['DB_PASSWORD']}@{os.environ['DB_HOST']}:{os.environ['DB_PORT']}/{os.environ['DB_NAME']}"
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.name}>"
+
+db.create_all()
+
+class UserResource(Resource):
+    def get(self, user_id):
+        user = User.query.get_or_404(user_id)
+        return {"id": user.id, "name": user.name}
+
+    def put(self, user_id):
+        user = User.query.get_or_404(user_id)
+        user.name = request.form["name"]
+        db.session.commit()
+        return {"id": user.id, "name": user.name}
+
+    def delete(self, user_id):
+        user = User.query.get_or_404(user_id)
+        db.session.delete(user)
+        db.session.commit()
+        return {"result": "success"}
+
+api.add_resource(UserResource, "/users/<int:user_id>")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
